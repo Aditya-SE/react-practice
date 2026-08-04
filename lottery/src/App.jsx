777a575dd3f17f5fe286072ba35e4a9629b325a5
Aditya-SE <adityapatil849703801313@gmail.com>
2026-08-04 13:48:36 +0530

    simple lottery game

---
+++ lottery/src/App.jsx
@@ -0,0 +1,14 @@
+import Lottery from "./Lottery";
+import TicketNum from "./TicketNum";
+import "./App.css";
+import Ticket from "./Ticket";
+
+function App() {
+  return (
+    <>
+      <Lottery n={3} winningSum={15} />
+    </>
+  );
+}
+
+export default App;
